f63d44d002eaddb96a24a5dbefcca8697adcd605
m-wie-mischa <mischa.google@gmx.de>
2025-05-19 19:43:06 +0200

    .

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
 import { io } from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js";
 
 let socket = io("https://web-friends.onrender.com/");
-let keys;
+let canvas;
 
 window.addEventListener('DOMContentLoaded', function() {
-    keys = document.getElementById("keys");
+
+    canvas = document.getElementById("canvas")
+
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+
+    let ctx = canvas.getContext("2d")
+
+
+    function update() {
+        requestAnimationFrame(update)
+        //...
+    }
+    update()
 });
 
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+})
+
 window.addEventListener("keydown", (e) => {
-    socket.emit("keyPressed", e.key)
+    socket.emit("keyDown", e.key)
     console.log(e.key)
 })
 
-socket.on("keyReceived", (id, key) => {
-    keys.innerHTML = `User: ${id} sent Key: ${key}`
+window.addEventListener("keyup", (e) => {
+    socket.emit("keyUp", e.key)
+    console.log(e.key)
 })
