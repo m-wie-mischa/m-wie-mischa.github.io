e9a34ee27c4645c36e83f4559006b9764319f5f5
m-wie-mischa <mischa.google@gmx.de>
2025-05-19 17:24:59 +0200

    .

---
+++ main.js
@@ -0,0 +1,17 @@
+import { io } from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js";
+
+let socket = io("https://web-friends.onrender.com:3000");
+let keys;
+
+window.addEventListener('DOMContentLoaded', function() {
+    keys = document.getElementById("keys");
+});
+
+window.addEventListener("keydown", (e) => {
+    socket.emit("keyPressed", e.key)
+    console.log(e.key)
+})
+
+socket.on("keyReceived", (id, key) => {
+    keys.innerHTML = `User: ${id} sent Key: ${key}`
+})
