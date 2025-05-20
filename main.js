2b18443b0b14588ac94f9e29748776aaae0f10ac
m-wie-mischa <mischa.google@gmx.de>
2025-05-20 13:11:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import { io } from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js";
 
 let socket = io("https://web-friends.onrender.com/");
 let canvas;
+let ctx;
 
 window.addEventListener('DOMContentLoaded', function() {
 
@@ -10,20 +11,18 @@ window.addEventListener('DOMContentLoaded', function() {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
 
-    let ctx = canvas.getContext("2d")
-
-
-    function update() {
-        requestAnimationFrame(update)
-        //...
-    }
-    update()
+    ctx = canvas.getContext("2d")
 });
 
 window.addEventListener("resize", () => {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
 })
+
+socket.on("update", (clients) => {
+    console.log("Clients connected", clients)
+})
+
 
 window.addEventListener("keydown", (e) => {
     socket.emit("keyDown", e.key)
